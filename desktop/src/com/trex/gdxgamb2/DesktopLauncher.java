9f0f71391715084d19eabd93b5f75ecaa29ee5de
JacksonTrexler <jtrexler25@gmail.com>
2023-08-04 00:47:02 -0400

    Base commit

---
+++ desktop/src/com/trex/gdxgamb2/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.trex.gdxgamb2;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.trex.gdxgamb2.GdxGamb2;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("gdx-gamb2");
+		new Lwjgl3Application(new GdxGamb2(), config);
+	}
+}
