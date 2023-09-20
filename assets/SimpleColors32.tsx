3e7f23c3374457724cabc2289240e746751066b8
JacksonTrexler <jtrexler25@gmail.com>
2023-09-20 17:42:06 -0400

    Starter tiled tileset and map to play with

---
+++ assets/SimpleColors32.tsx
@@ -0,0 +1,33 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="SimpleColors32" tilewidth="32" tileheight="32" tilecount="10" columns="1">
+ <tile id="0">
+  <image width="32" height="32" source="../assets/tile/basic/black32.png"/>
+ </tile>
+ <tile id="1">
+  <image width="32" height="32" source="../assets/tile/basic/blue32.png"/>
+ </tile>
+ <tile id="2">
+  <image width="32" height="32" source="../assets/tile/basic/darkgreen32.png"/>
+ </tile>
+ <tile id="3">
+  <image width="32" height="32" source="../assets/tile/basic/gray32.png"/>
+ </tile>
+ <tile id="4">
+  <image width="32" height="32" source="../assets/tile/basic/green32.png"/>
+ </tile>
+ <tile id="5">
+  <image width="32" height="32" source="../assets/tile/basic/lightblue.png"/>
+ </tile>
+ <tile id="6">
+  <image width="32" height="32" source="../assets/tile/basic/purple32.png"/>
+ </tile>
+ <tile id="7">
+  <image width="32" height="32" source="../assets/tile/basic/red32.png"/>
+ </tile>
+ <tile id="8">
+  <image width="32" height="32" source="../assets/tile/basic/white32.png"/>
+ </tile>
+ <tile id="9">
+  <image width="32" height="32" source="../assets/tile/basic/yellow32.png"/>
+ </tile>
+</tileset>
